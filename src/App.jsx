15d8d780efa6cc753beeed86a18f2561a280f147
tTanya98 <chichkinat98@gmail.com>
2023-05-26 22:13:09 +0300

    updt

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/navigation/Navigation';
 import { lazy, Suspense } from 'react';
 import Container from './components/container/Container';
