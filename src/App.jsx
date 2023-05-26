ee53e9a5933552f17dcc4a6b9b37631db2333020
tTanya98 <chichkinat98@gmail.com>
2023-05-26 21:39:13 +0300

    updt folders

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { Route, Switch } from 'react-router-dom';
+import Navigation from './components/navigation/Navigation';
+import { lazy, Suspense } from 'react';
+import Container from './components/container/Container';
+import Loader from 'components/loader/Loader';
+
+const HomePage = lazy(() =>
+  import('./pages/HomePage')
+);
+const MoviesPage = lazy(() =>
+  import('./pages/MoviesPage')
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    './pages/MovieDetailsPage/MovieDetailsPage'
+  )
+);
+const NotFoundView = lazy(() =>
+  import('./pages/NotFound')
+);
+
+export default function App() {
+  return (
+    <Container>
+      <Navigation />
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/movies">
+            <MoviesPage />
+          </Route>
+          <Route path="/movies/:movieId">
+            <MovieDetailsPage />
+          </Route>
+
+          <Route>
+            <NotFoundView path="*" />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Container>
+  );
+}
