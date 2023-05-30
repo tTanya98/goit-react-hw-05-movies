5bc3abb4a101c6eafc283d122105aaf9f9b6a519
tTanya98 <114945668+tTanya98@users.noreply.github.com>
2023-05-30 13:40:24 +0000

    Deploying to gh-pages from @ tTanya98/goit-react-hw-05-movies@16d70fbc269fbe051128f683345235117ef4bb22 🚀

--- static/js/922.09b55980.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[922],{922:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(689),o=n(184);function u(){var e=(0,r.s0)();return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h2",{children:"404 Not Found "}),(0,o.jsx)("button",{type:"button",onClick:function(){e.push("/")},children:"Go home"})]})}}}]);
-//# sourceMappingURL=922.09b55980.chunk.js.map
